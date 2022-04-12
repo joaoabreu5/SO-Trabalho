6082cf3e1b155ce3985b4f3dbca90a988ab3be40
joaoabreu4 <joaomachadoabreu@hotmail.com>
2022-04-12 19:34:42 +0100

    Correção função "proc-file". PS: Ricardo Sousa, és o REI!

--- cliente.c
+++ cliente.c
@@ -9,18 +9,49 @@
 
 char *get_path(char *exec)
 {
-    char *new_exec = strdup(exec);
     char *path = calloc(SIZE, sizeof(char));
     strcat(path, "./SDStore/");
-    strcat(path, new_exec);
+    strcat(path, exec);
     return path;
+}
+
+void close_npipes(int n, int pipe_array[n][2])
+{
+    for (int i = 0; i < n; i++)
+    {
+        close(pipe_array[i][0]);
+        close(pipe_array[i][1]);
+    }
+}
+
+void free_command_array(char ***comandos, int N)
+{
+    int i, j;
+    for(i=0; i<N; i++)
+    {
+        for(j=0; j<2; j++)
+        {
+            if (comandos[i][j] != NULL)
+            {
+                free(comandos[i][j]);
+            }
+        }
+        if (comandos[i] != NULL)
+        {
+            free(comandos[i]);
+        }
+    }
+    if(comandos != NULL)
+    {
+        free(comandos);
+    }
 }
 
 void proc_file(int argc, char* argv[])
 {
-    int i, return_exec, status;
-    
-    char *path = NULL;
+    int command_number = argc-4;
+    int i, j, t, pid, r_exec, pipes[command_number-1][2];
+    char *path = NULL, ***comandos = NULL;
 
     int fd_in = open(argv[2], O_RDONLY);
     int fd_out = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, 0666);
@@ -34,31 +65,81 @@ void proc_file(int argc, char* argv[])
     close(fd_in);
     close(fd_out);
 
-    for(i=4; i<argc; i++)
+    if (command_number == 1)
     {
         if (fork() == 0)
         {
+            i = 4;
             path = get_path(argv[i]);
-            return_exec = execl(path, argv[i], NULL);
+            r_exec = execl(path, argv[i], NULL);
             free(path);
-            _exit(return_exec);
+            _exit(r_exec);
         }
         else
         {
-            wait(&status);
+            wait(NULL);
         }
+    }
+    else if (command_number > 1)
+    {
+        comandos = malloc(command_number*sizeof(char **));
+        for(i=0; i<command_number; i++)
+        {
+            comandos[i] = malloc(3*sizeof(char **));
+            comandos[i][0] = get_path(argv[i+4]);
+            comandos[i][1] = strdup(argv[i+4]);
+            comandos[i][2] = NULL;
+        }
+
+        for(t=0; t<command_number-1; t++)
+        {
+            pipe(pipes[t]);
+        }
+
+        if ((pid = fork()) == 0)
+        {
+            dup2(pipes[0][1], 1);
+            close_npipes(command_number-1, pipes);
+            execvp(comandos[0][0], comandos[0]);
+        }
+
+        for(i=1; i<command_number-1; i++)
+        {
+            if ((pid = fork()) == 0)
+            {
+                dup2(pipes[i-1][0], 0);
+                dup2(pipes[i][1], 1);
+                close_npipes(command_number-1, pipes);
+                r_exec = execvp(comandos[i][0], comandos[i]);
+                _exit(r_exec);
+            }
+        }
+
+        if ((pid = fork()) == 0)
+        {
+            dup2(pipes[i-1][0], 0);
+            close_npipes(command_number-1, pipes);
+            r_exec = execvp(comandos[i][0], comandos[i]);
+            _exit(r_exec);
+        }
+
+        close_npipes(command_number-1, pipes);
+
+        for(j=0; j<command_number; j++)
+        {
+            wait(NULL);
+        }
+
+        free_command_array(comandos, command_number);
     }
 
     dup2(fd_0, 0);
     dup2(fd_1, 1);
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    char *arg = calloc(10, sizeof(char));
-    strcpy(arg, "proc-file");
-
-    if (strcmp(argv[1], "proc-file") == 0)
+    if (argc > 1 && strcmp(argv[1], "proc-file") == 0)
     {
         proc_file(argc, argv);
     }
